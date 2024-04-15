dfaacd704b351eca8b2cf15aa759cb03c278033f
Nick Oates <nick@nickoates.com>
2024-04-15 11:43:11 -0700

    Image cover on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -115,7 +115,7 @@ export default function Home() {
 						src={blobURL}
 						unoptimized
 						fill
-						className="object-contain min-h-16"
+						className="lg:object-contain object-cover min-h-16"
 						alt="Uploaded image"
 					/>
 				)}
