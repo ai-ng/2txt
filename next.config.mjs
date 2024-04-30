2f6ea9f5afee0d254443ec37df9ede79ee0755f7
Nick Oates <nick@nickoates.com>
2024-04-30 09:35:46 -0700

    Prevent website from being iframed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: "frame-ancestors 'none'",
+					},
+				],
+			},
+		];
+	},
+};
 
 export default nextConfig;
